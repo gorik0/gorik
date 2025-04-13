9aa455f138811a31664163dc3f9aa6cd5caf4058
gorik0 <delavarus@gmail.com>
2025-04-13 16:34:43 +0300

    server_test

--- znet/server.go
+++ znet/server.go
@@ -14,13 +14,13 @@ type Server struct {
 }
 
 // Serve implements ziface.Iserver.
-func (s Server) Serve() {
+func (s *Server) Serve() {
 	s.Start()
 	select {}
 }
 
 // Start implements ziface.Iserver.
-func (s Server) Start() {
+func (s *Server) Start() {
 
 	fmt.Printf("[sTart]sevrer listen on port %d in addr %s .... \n", s.Port, s.IP)
 
@@ -37,41 +37,41 @@ func (s Server) Start() {
 	}
 	fmt.Println("start Zinx server  ", s.Name, " succ, now listening...")
 
-        // 3. Start the server network connection business.
-        for {
-            // 3.1. Block and wait for client connection requests.
-            conn, err := listener.AcceptTCP()
-            if err != nil {
-                fmt.Println("Accept err ", err)
-                continue
+	// 3. Start the server network connection business.
+	for {
+		// 3.1. Block and wait for client connection requests.
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			fmt.Println("Accept err ", err)
+			continue
 
-            }
-			go func() {
-                // Continuously loop to get data from the client.
-                for {
-                    buf := make([]byte, 512)
-                    cnt, err := conn.Read(buf)
-                    if err != nil {
-                        fmt.Println("recv buf err ", err)
-                        continue
+		}
+		go func() {
+			// Continuously loop to get data from the client.
+			for {
+				buf := make([]byte, 512)
+				cnt, err := conn.Read(buf)
+				if err != nil {
+					fmt.Println("recv buf err ", err)
+					continue
 
-                    }
-                    // Echo back the received data.
-                    if _, err := conn.Write(buf[:cnt]); err != nil {
-                        fmt.Println("write back buf err ", err)
-                        continue
+				}
+				// Echo back the received data.
+				if _, err := conn.Write(buf[:cnt]); err != nil {
+					fmt.Println("write back buf err ", err)
+					continue
 
-                    }
+				}
 
-                }
+			}
 
-            }()
+		}()
 
-
+	}
 }
 
 // Stop implements ziface.Iserver.
-func (s Server) Stop() {
+func (s *Server) Stop() {
 	fmt.Printf("[STOOP server %s \n]", s.Name)
 	// todo
 
@@ -85,5 +85,3 @@ func NewServer(name string) ziface.Iserver {
 		Port:      7777,
 	}
 }
-
-var _ ziface.Iserver = Server{}
---
+++ znet/server_test.go
@@ -0,0 +1,40 @@
+package znet
+
+import (
+	"fmt"
+	"net"
+	"testing"
+	"time"
+)
+
+func ClientTest() {
+	fmt.Println("Starting clietn !")
+	time.Sleep(time.Second * 3)
+	conn, err := net.Dial("tcp", "127.0.0.1:7777")
+	if err != nil {
+		println("Client start err :::; ", err)
+	}
+
+	for {
+		_, err := conn.Write([]byte("HELO server !!! "))
+		if err != nil {
+			println("error while writing ", err)
+		}
+		buffer := make([]byte, 512)
+		cnt, err := conn.Read(buffer)
+		if err != nil {
+			println("error while reading .... ", err)
+		}
+		fmt.Printf("Succesfully reading .... msg:%s , cnt = %d \n", buffer, cnt)
+		time.Sleep(time.Second * 1)
+	}
+
+}
+
+func TestSetrver(t *testing.T) {
+	s := NewServer("goriko")
+
+	go ClientTest()
+
+	s.Start()
+}
